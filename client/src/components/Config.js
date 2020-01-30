0667dc038532a8cf8f8023f478c16be0e4e32353
technopreneur <premnath.borkar@vpstechub.com>
2020-01-30 18:15:39 +0530

    First Commit

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,6 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_KEY = 'a004ac8967c445d24a88204dc922893f';
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const IMAGE_URL = 'http://image.tmdb.org/t/p/';
